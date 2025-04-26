427edc143db4468ff7fdec70a6a2a9d65fde0aca
David Tamang <davidtamang.upwork@gmail.com>
2025-04-26 09:02:36 +0545

    Verification event handlers registered.

--- django-projects/connectify/user_management/apps.py
+++ django-projects/connectify/user_management/apps.py
@@ -8,3 +8,4 @@ class UserManagementConfig(AppConfig):
 
     def ready(self):
         importlib.import_module("user_management.user.service_layer.event_handlers")
+        importlib.import_module("user_management.verification.service_layer.event_handlers")

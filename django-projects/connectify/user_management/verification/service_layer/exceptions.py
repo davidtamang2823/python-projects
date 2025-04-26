8c9a8a8195d0a9e1d61dd72efb70ee285c9b73d4
David Tamang <davidtamang.upwork@gmail.com>
2025-04-26 09:15:57 +0545

    User verification service layer exceptions added.

---
+++ django-projects/connectify/user_management/verification/service_layer/exceptions.py
@@ -0,0 +1,33 @@
+class TokenExpired(Exception):
+
+
+    def __str__(self):
+        return "Your token has been expired. Please request again."
+
+
+class TokenNotFound(Exception):
+
+
+    def __str__(self):
+        return "Token not found."
+    
+
+class UserAlreadyVerified(Exception):
+
+
+    def __str__(self):
+        return "User already verified."
+    
+
+class UnregisteredUser(Exception):
+
+
+    def __str__(self):
+        return "User Not Found."
+    
+
+class TokenNotExpired(Exception):
+
+
+    def __str__(self):
+        return "User verification email has been sent already."

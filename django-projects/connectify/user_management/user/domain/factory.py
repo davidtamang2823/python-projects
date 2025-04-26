f78c21e815a1456372123cdf7421242a40d5e367
David Tamang <davidtamang.upwork@gmail.com>
2025-04-26 09:06:51 +0545

    Token parameter added to factory method.

--- django-projects/connectify/user_management/user/domain/factory.py
+++ django-projects/connectify/user_management/user/domain/factory.py
@@ -1,4 +1,3 @@
-import secrets
 from abc import ABC, abstractmethod
 from user_management.user.domain import model as user_domain_model
 from user_management.user.domain import events as user_events
@@ -7,39 +6,40 @@ from user_management.user.common import constants as user_constants
 class AbstractUserFactory(ABC):
 
 
-    @staticmethod
     @abstractmethod
     def create_user(
+        self,
         email: str, 
         password: str, 
         username: str, 
         first_name: str, 
         last_name: str, 
+        token: str,
         is_active: bool = False,
         is_superuser: bool = False,
         is_staff: bool = False, 
     ) -> user_domain_model.User:
         raise NotImplementedError
     
-    @staticmethod
     @abstractmethod
-    def update_full_name(user_id: int, first_name: str, last_name: str) -> user_domain_model.User:
+    def update_full_name(self, user_id: int, first_name: str, last_name: str) -> user_domain_model.User:
         raise NotImplementedError
 
 
 class UserFactory(AbstractUserFactory):
 
 
-    @staticmethod
     def create_user(
+        self,
         email: str, 
         password: str, 
         username: str, 
         first_name: str, 
-        last_name: str, 
+        last_name: str,
+        token: str,
         is_active: bool = False,
         is_superuser: bool = False,
-        is_staff: bool = False, 
+        is_staff: bool = False,
     ) -> user_domain_model.User:
         return user_domain_model.User(
             email = email,
@@ -51,18 +51,17 @@ class UserFactory(AbstractUserFactory):
             is_superuser = is_superuser,
             is_staff = is_staff,
             events = [
-                user_events.SendUserRegistrationConfirmationEmail(
+                user_events.SendUserVerificationEmail(
                     event_type=user_constants.USER_REGISTRATION,
                     source=user_constants.EVENT_SOURCE,
                     send_to=email,
                     send_to_username=username,
-                    verification_token=secrets.token_urlsafe(64),
+                    verification_token=token,
                 )
             ]
         )
     
-    @staticmethod
-    def update_full_name(user_id: int, first_name: str, last_name: str) -> user_domain_model.User:
+    def update_full_name(self, user_id: int, first_name: str, last_name: str) -> user_domain_model.User:
         return user_domain_model.User(
             id = user_id,
             first_name = first_name,

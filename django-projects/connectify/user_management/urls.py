bf4a59d29058905d15302232ae6a8e3a82f6da09
David Tamang <davidtamang.upwork@gmail.com>
2025-04-27 13:46:28 +0545

    Registering user verification viewset to router.

--- django-projects/connectify/user_management/urls.py
+++ django-projects/connectify/user_management/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
 from user_management.user.entrypoints.viewsets import UserViewSet
+from user_management.verification.entrypoints.viewsets import VerificationViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='apiuser')
-
+router.register(r'verifications', VerificationViewSet, basename='apiuserverification')
 urlpatterns = router.urls

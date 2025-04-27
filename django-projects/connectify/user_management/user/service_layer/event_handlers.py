d5d18ffe151196eb28c9c4eafc421c18559ba103
David Tamang <davidtamang.upwork@gmail.com>
2025-04-27 13:48:52 +0545

    Renaming handler name.

--- django-projects/connectify/user_management/user/service_layer/event_handlers.py
+++ django-projects/connectify/user_management/user/service_layer/event_handlers.py
@@ -4,5 +4,5 @@ from user_management.user.domain import events as user_events
 from user_management.tasks import send_verification_email_task
 
 @message_bus.register_handler(event_type = constants.USER_REGISTRATION)
-def handle_user_registration_confirmation(event: user_events.SendUserVerificationEmail):
+def handle_user_verification(event: user_events.SendUserVerificationEmail):
     send_verification_email_task.delay(event.send_to, event.send_to_username, event.verification_token)

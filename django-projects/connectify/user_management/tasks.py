0c525389279f1cce65797d78769d9a0bd027e8d8
David Tamang <davidtamang.upwork@gmail.com>
2025-04-27 13:45:51 +0545

    Changed send_to parameter type and renaming template name.

--- django-projects/connectify/user_management/tasks.py
+++ django-projects/connectify/user_management/tasks.py
@@ -1,10 +1,11 @@
+from typing import List
 from celery import shared_task
 from django.core.mail import EmailMultiAlternatives
 from django.utils.html import strip_tags
 from django.template.loader import render_to_string
 
 
-def send_verification_email(send_to: str, subject: str, context, template_name: str):
+def send_verification_email(send_to: List[str], subject: str, context, template_name: str):
     html_content = render_to_string(
         template_name=template_name,
         context=context
@@ -23,13 +24,13 @@ def send_verification_email(send_to: str, subject: str, context, template_name: 
 @shared_task
 def send_verification_email_task(send_to: str, send_to_username: str, verification_token: str):
     subject = "Verify your email"
-    template_name = 'verifications/templates/user_verification_email_template.html'
+    template_name = 'verification/user_verification_email_template.html'
     context = {
         'username': send_to_username,
         'verification_url': f"{verification_token}"
     }
     send_verification_email(
-        send_to=send_to, 
+        send_to=[send_to],
         subject=subject,
         context=context,
         template_name=template_name

7c86c3fd127deee141c1ee34519dd2405d0917ae
David Tamang <davidtamang.upwork@gmail.com>
2025-04-29 14:20:55 +0545

    Password reset event class.

--- django-projects/connectify/user_management/verification/domain/events.py
+++ django-projects/connectify/user_management/verification/domain/events.py
@@ -4,3 +4,9 @@ class ResendUserVerificationEmail(Event):
     send_to: str
     send_to_username: str
     verification_token: str
+
+
+class SendPasswordResetEmail(Event):
+    send_to: str
+    send_to_username: str
+    verification_token: str

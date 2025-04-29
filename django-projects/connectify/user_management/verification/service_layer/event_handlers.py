7f581bad1e5e0e6649523542eca827e4a2f7ab9c
David Tamang <davidtamang.upwork@gmail.com>
2025-04-29 14:22:15 +0545

    Password reset event handler.

--- django-projects/connectify/user_management/verification/service_layer/event_handlers.py
+++ django-projects/connectify/user_management/verification/service_layer/event_handlers.py
@@ -1,8 +1,12 @@
 from events import message_bus
 from user_management.verification.common import constants
 from user_management.verification.domain import events as user_verification_events
-from user_management.tasks import send_verification_email_task
+from user_management.tasks import send_verification_email_task, send_password_reset_email_task
 
 @message_bus.register_handler(event_type = constants.RESEND_VERIFICATION_EMAIL)
 def handle_resend_user_verification(event: user_verification_events.ResendUserVerificationEmail):
     send_verification_email_task.delay(event.send_to, event.send_to_username, event.verification_token)
+
+@message_bus.register_handler(event_type = constants.SEND_PASSWORD_RESET_EMAIL)
+def handle_send_password_reset_email(event: user_verification_events.SendPasswordResetEmail):
+    send_password_reset_email_task.delay(event.send_to, event.send_to_username, event.verification_token)

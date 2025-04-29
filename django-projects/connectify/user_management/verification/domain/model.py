61acfde031d2b2b9c8835e76764955823e5ab792
David Tamang <davidtamang.upwork@gmail.com>
2025-04-29 14:21:36 +0545

    Password reset domain model class.

--- django-projects/connectify/user_management/verification/domain/model.py
+++ django-projects/connectify/user_management/verification/domain/model.py
@@ -1,6 +1,7 @@
 from typing import List, Optional
 from pydantic import BaseModel, ConfigDict, field_validator, Field
 from user_management.verification.domain import exceptions as user_verification_exceptions
+from user_management.common import exceptions as user_management_common_exceptions
 
 class Base(BaseModel):
 
@@ -17,6 +18,30 @@ class UserVerification(BaseModel):
 
     token: str
     events: Optional[List] = Field(default_factory=list)
+
+    @field_validator('token', mode='after')
+    @classmethod
+    def validate_username(cls, value):
+        if value is not None and (len(value) != 64):
+            raise user_verification_exceptions.InvalidToken
+        return value
+    
+
+class PasswordReset(BaseModel):
+
+
+    token: str
+    email: Optional[str] = None
+    password: Optional[str] = None
+    events: Optional[List] = Field(default_factory=list)
+
+    @field_validator('password', mode='after')
+    @classmethod
+    def validate_password(cls, value):
+        if value is not None and len(value) != 8:
+            raise user_management_common_exceptions.InvalidPasswordLength
+        return value
+
 
     @field_validator('token', mode='after')
     @classmethod

a288ef2c59a13c35b922ec510ac06639537e79ff
David Tamang <davidtamang.upwork@gmail.com>
2025-04-29 14:14:49 +0545

    Password reset send email celery task added.

--- django-projects/connectify/user_management/tasks.py
+++ django-projects/connectify/user_management/tasks.py
@@ -35,3 +35,19 @@ def send_verification_email_task(send_to: str, send_to_username: str, verificati
         context=context,
         template_name=template_name
     )
+
+@shared_task
+def send_password_reset_email_task(send_to: str, send_to_username: str, verification_token: str):
+    subject = "Password reset email"
+    template_name = "verification/password_reset_email_template.html"
+    context = {
+        'username': send_to_username,
+        'verification_token': verification_token
+    }
+
+    send_verification_email(
+        send_to=[send_to],
+        subject=subject,
+        context=context,
+        template_name=template_name
+    )

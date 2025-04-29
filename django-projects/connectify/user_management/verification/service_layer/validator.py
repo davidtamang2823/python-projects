1a83bef2361b816d99b773ef9523faef5fd8bd6c
David Tamang <davidtamang.upwork@gmail.com>
2025-04-29 14:23:11 +0545

    Password reset validator class.

--- django-projects/connectify/user_management/verification/service_layer/validator.py
+++ django-projects/connectify/user_management/verification/service_layer/validator.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 from user_management.verification.service_layer import exceptions as service_layer_exceptions
 
 
-class TokenValidator:
+class CommonTokenValidator:
 
 
     def validate_token_is_expired(self, created_at: datetime):
@@ -19,6 +19,9 @@ class TokenValidator:
         if (timezone.now() - created_at) < timedelta(minutes=15):
             raise service_layer_exceptions.TokenNotExpired
 
+class TokenValidator(CommonTokenValidator):
+
+
     def validate_user_registered_or_not(self, token_details: Dict):
         if token_details is None:
             raise service_layer_exceptions.UnregisteredUser
@@ -26,3 +29,7 @@ class TokenValidator:
     def validate_user_is_verified_or_not(self, is_verified: bool):
         if is_verified is True:
             raise service_layer_exceptions.UserAlreadyVerified
+
+
+class PasswordResetValidator(CommonTokenValidator):
+    """Method Not Implemented"""

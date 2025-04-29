0eb2403196d05dbe60df7d78462547a60b974106
David Tamang <davidtamang.upwork@gmail.com>
2025-04-29 14:19:40 +0545

    New orm class created.

--- django-projects/connectify/user_management/verification/adapters/orm.py
+++ django-projects/connectify/user_management/verification/adapters/orm.py
@@ -12,3 +12,12 @@ class UserVerification(models.Model):
 
     class Meta:
         default_permissions = ()
+
+
+class PasswordResetVerification(models.Model):
+
+
+    reset_at = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    token = models.CharField(max_length=64, unique=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='password_reset')

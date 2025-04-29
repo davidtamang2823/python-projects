eb81a008cb329ccaa86c4230a792879c02cae985
David Tamang <davidtamang.upwork@gmail.com>
2025-04-29 14:15:23 +0545

    Password reset viewset register to router.

--- django-projects/connectify/user_management/urls.py
+++ django-projects/connectify/user_management/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
 
 from user_management.user.entrypoints.viewsets import UserViewSet
-from user_management.verification.entrypoints.viewsets import VerificationViewSet
+from user_management.verification.entrypoints.viewsets import VerificationViewSet, PasswordResetViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='apiuser')
 router.register(r'verifications', VerificationViewSet, basename='apiuserverification')
+router.register(r'passwordresets', PasswordResetViewSet, basename='apipasswordreset')
 urlpatterns = router.urls

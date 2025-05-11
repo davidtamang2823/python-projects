fe30079b751fb8611beef0be4d9cd7d37d88883b
David Tamang <davidtamang.upwork@gmail.com>
2025-05-11 07:57:37 +0545

    websocket protocol.

--- django-projects/connectify/connectify/asgi.py
+++ django-projects/connectify/connectify/asgi.py
@@ -8,14 +8,23 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-
-from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'connectify.settings')
 
+django_asgi_app = get_asgi_application()
+
+from chat.routing import websocket_urlpatterns as chat_websocket_urlpatterns
+
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application()
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            URLRouter(
+                chat_websocket_urlpatterns
+            )
+        )
     }
 )

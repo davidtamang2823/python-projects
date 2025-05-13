cc9ce1352f1f2e8074f753706cd1ade6dbe43b6c
David Tamang <davidtamang.upwork@gmail.com>
2025-05-13 21:08:27 +0545

    domain factory.

---
+++ django-projects/connectify/chat/message/domain/factory.py
@@ -0,0 +1,28 @@
+from chat.message.domain import exceptions as domain_layer_exceptions
+from chat.message.domain.model import PrivateChatModel
+from abc import ABC, abstractmethod
+
+class AbstractPrivateChatFactory(ABC):
+
+
+    @abstractmethod
+    def create(self, sender_id: int, receiver_id: int, content: str) -> PrivateChatModel:
+        raise NotImplementedError
+    
+
+class PrivateChatFactory(AbstractPrivateChatFactory):
+
+
+    def create_private_chat(self, sender_id: int, receiver_id: int, content: str) -> PrivateChatModel:
+        return PrivateChatModel(
+            sender_id=sender_id, 
+            receiver_id=receiver_id, 
+            content=content
+        )
+    
+    def update_private_chat(self, sender_id: int, receiver_id: int, content: str) -> PrivateChatModel:
+        return PrivateChatModel(
+            sender_id=sender_id, 
+            receiver_id=receiver_id, 
+            content=content
+        )

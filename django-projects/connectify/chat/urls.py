b273dbfbe31deb53a182554bcb57f01e0281cd12
David Tamang <davidtamang.upwork@gmail.com>
2025-05-17 09:33:18 +0545

    Private chat viewset registered to router.

--- django-projects/connectify/chat/urls.py
+++ django-projects/connectify/chat/urls.py
@@ -1,8 +1,10 @@
 from rest_framework.routers import DefaultRouter
 
 from chat.friend.entrypoints.viewsets import UserFriendViewSet
+from chat.message.entrypoints.viewsets import PrivateChatViewSet
 
 router = DefaultRouter()
 router.register(r'friends', UserFriendViewSet, basename='user_friend')
+router.register(r'private_chats', PrivateChatViewSet, basename='private_chat')
 
 urlpatterns = router.urls

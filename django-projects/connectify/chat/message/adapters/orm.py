af12ca883742efc573c2b6820f7ec2687fd3b0a4
David Tamang <davidtamang.upwork@gmail.com>
2025-05-17 09:33:42 +0545

    is_deleted field added to private message orm class.

--- django-projects/connectify/chat/message/adapters/orm.py
+++ django-projects/connectify/chat/message/adapters/orm.py
@@ -2,10 +2,11 @@ from django.db import models
 from django.conf import settings
 
 class PrivateMessage(models.Model):
-    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, related_name='sent_messages')
-    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, related_name='received_messages')
-    content = models.TextField(max_length=20000, min_length=1)
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name='sent_messages')
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name='received_messages')
+    content = models.TextField(max_length=20000)
     is_seen = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 

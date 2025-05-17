f614cb8501ac5bb008460a10db3825455fefd4b0
David Tamang <davidtamang.upwork@gmail.com>
2025-05-17 09:36:38 +0545

    validator updated.

--- django-projects/connectify/chat/message/service_layer/validator.py
+++ django-projects/connectify/chat/message/service_layer/validator.py
@@ -17,3 +17,16 @@ class PrivateChatValidator:
             raise service_layer_exceptions.UserFriendRequestPending
         elif status == 'rejected':
             raise service_layer_exceptions.UserFriendRequestRejected
+        
+
+    def validate_can_update_message(self, sender_id: int, existing_sender_id: int):
+        if sender_id != existing_sender_id:
+            raise service_layer_exceptions.UserNotSender("You are not the sender of this message. Cannot update message.")
+
+    def validate_can_delete_message(self, sender_id: int, existing_sender_id: int):
+        if int(sender_id) != existing_sender_id:
+            raise service_layer_exceptions.UserNotSender('Cannot delete message. You are not the sender of this message.')
+
+    def validate_message_existence(self, message_details: Dict):
+        if message_details is None:
+            raise service_layer_exceptions.MessageNotFound

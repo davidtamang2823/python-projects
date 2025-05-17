bc863d10428cd4799ee81c06e5ac2c530b33e30d
David Tamang <davidtamang.upwork@gmail.com>
2025-05-17 09:35:16 +0545

    exception updated.

--- django-projects/connectify/chat/message/service_layer/exceptions.py
+++ django-projects/connectify/chat/message/service_layer/exceptions.py
@@ -24,3 +24,21 @@ class UserFriendRequestRejected(Exception):
 
     def __str__(self):
         return "You have a rejected friend request with this user. Cannot send message."
+    
+class UserNotSender(Exception):
+
+
+    """
+    Exception raised when the user is not the sender of the message.
+    """
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
+
+class MessageNotFound(Exception):
+
+    def __str__(self):
+        return "Message not found."
